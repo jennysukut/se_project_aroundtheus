98f73b2e14510a7b1ce89099823df7b144e2146c
JennySukut <jennysukut@gmail.com>
2024-01-19 09:45:36 +0200

    Edited JS File

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,29 @@
-const = initialCards [
+const = initialCards[
     { 
         name: "Dolomites",
-        link:"https://unsplash.com/photos/a-field-of-flowers-with-mountains-in-the-background-UM6icC4s4gQ"
-    }
+        link:"https://unsplash.com/photos/a-field-of-flowers-with-mountains-in-the-background-UM6icC4s4gQ",
+    },
     {
         name: "Hallstatt",
         link:"https://unsplash.com/photos/a-scenic-view-of-a-town-on-a-lake-with-mountains-in-the-background-UW3oVK-HzbM",
-    }
+    },
     {
         name:"Bryce Canyon",
         link:"https://unsplash.com/photos/brown-rock-formation-under-blue-sky-during-daytime-iFFUbr-4ByU",
-    }
+    },
     {
         name:"Seceda",
         link:"https://unsplash.com/photos/a-rocky-mountain-with-snow-C229nrkAgUs",
-    }
+    },
     {
         name:"Santorini",
         link:"https://unsplash.com/photos/white-and-blue-concrete-building-near-body-of-water-during-daytime-vF0l0bqLRKY",
-    }
+    },
     {
         name:"Biertan",
         link:"https://unsplash.com/photos/aerial-view-of-city-during-daytime-VmIkAvcCdPo",
     }
-]
+];
 
 
 //Dolomites: https://unsplash.com/photos/a-field-of-flowers-with-mountains-in-the-background-UM6icC4s4gQ
