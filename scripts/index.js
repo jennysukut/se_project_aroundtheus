17d354919adbe43438340209fdd0eb60d268fca7
JennySukut <jennysukut@gmail.com>
2024-01-19 09:40:01 +0200

    Added JS Array of Objects

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+const = initialCards [
+    { 
+        name: "Dolomites",
+        link:"https://unsplash.com/photos/a-field-of-flowers-with-mountains-in-the-background-UM6icC4s4gQ"
+    }
+    {
+        name: "Hallstatt",
+        link:"https://unsplash.com/photos/a-scenic-view-of-a-town-on-a-lake-with-mountains-in-the-background-UW3oVK-HzbM",
+    }
+    {
+        name:"Bryce Canyon",
+        link:"https://unsplash.com/photos/brown-rock-formation-under-blue-sky-during-daytime-iFFUbr-4ByU",
+    }
+    {
+        name:"Seceda",
+        link:"https://unsplash.com/photos/a-rocky-mountain-with-snow-C229nrkAgUs",
+    }
+    {
+        name:"Santorini",
+        link:"https://unsplash.com/photos/white-and-blue-concrete-building-near-body-of-water-during-daytime-vF0l0bqLRKY",
+    }
+    {
+        name:"Biertan",
+        link:"https://unsplash.com/photos/aerial-view-of-city-during-daytime-VmIkAvcCdPo",
+    }
+]
+
+
+//Dolomites: https://unsplash.com/photos/a-field-of-flowers-with-mountains-in-the-background-UM6icC4s4gQ
+//Hallstatt: https://unsplash.com/photos/a-scenic-view-of-a-town-on-a-lake-with-mountains-in-the-background-UW3oVK-HzbM
+//Bryce Canyon 2: https://unsplash.com/photos/brown-rock-formation-under-blue-sky-during-daytime-iFFUbr-4ByU
+//Seceda: https://unsplash.com/photos/a-rocky-mountain-with-snow-C229nrkAgUs
+//Pale di San Martino COW: https://unsplash.com/photos/brown-cow-on-green-grass-field-during-daytime-9rRSkQtQPzE
+//Santorini: https://unsplash.com/photos/white-and-blue-concrete-building-near-body-of-water-during-daytime-vF0l0bqLRKY
+//Biertan: https://unsplash.com/photos/aerial-view-of-city-during-daytime-VmIkAvcCdPo
