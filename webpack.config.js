d2165d387eade09ece1bfbb82f25b87c07dd83fd
JennySukut <jennysukut@gmail.com>
2024-03-16 18:21:22 +0100

    Fix for Project 8 Submission

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
-      favicon: "..src/images/favicon.ico",
+      favicon: "./src/images/favicon.ico",
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
